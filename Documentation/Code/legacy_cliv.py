b42af36577fadce7df527e380afc6ea07677e1a2
Tariq-Chameleon <tariq.russell.t@gmail.com>
2018-03-31 19:42:08 +0200

    New implementation (cli_visualizer.py) has been implemented with playwav.py. Removed the old visualizer and was put in legacy_cliv.py

---
+++ Documentation/Code/legacy_cliv.py
@@ -0,0 +1,19 @@
+# print lables: Frequency Level Vector.....f0 range, f1 range,....,etc
+print("                           0-30         30          150          320           600          1100         3k           5k           7.1k         10.1k")
+print("Freq. LVL Vect             f0R          f1R         f2R          f3R           f4R          f5R          f6R          f7R          f8R          f9R")
+
+for i in range (0,FREQ_DOMAIN_NUM_RANGES):
+            if FREQ_DOMAIN[i] == 0:
+                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[0]
+            elif FREQ_DOMAIN[i] <=4 and FREQ_DOMAIN[i] > 0:
+                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[1]
+            elif FREQ_DOMAIN[i] <=8 and FREQ_DOMAIN[i] > 4:
+                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[2]
+            elif FREQ_DOMAIN[i] <=12 and FREQ_DOMAIN[i] > 8:
+                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[3]
+            elif FREQ_DOMAIN[i] <=16 and FREQ_DOMAIN[i] > 12:
+                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[4]
+            elif FREQ_DOMAIN[i] <=FREQ_DOMAIN_CEILING and FREQ_DOMAIN[i] > 16:
+                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[5]
+
+       OS.system("echo -en " + str(FREQ_DOMAIN) + str(FREQ_LVL_CUR[0]) + str(FREQ_LVL_CUR[1]) + str(FREQ_LVL_CUR[2]) + str(FREQ_LVL_CUR[3]) +str(FREQ_LVL_CUR[4]) +str(FREQ_LVL_CUR[5]) +str(FREQ_LVL_CUR[6]) + str(FREQ_LVL_CUR[7]) + str(FREQ_LVL_CUR[8])+ str(FREQ_LVL_CUR[9]) + "\r")
--- Documentation/Code/playwav.py
+++ Documentation/Code/playwav.py
@@ -1,4 +1,5 @@
 # Written by Tariq A.
+#Repository ---> github.com/tariq-chameleon/ASVS.git
 # Thanks to https://www.rototron.info for inspiring me to use the numpy FFT
 # https://www.wavtones.com/functiongenerator.php allowed me to generate audio signals
 # https://www.audiocheck.net/audiofrequencysignalgenerator_sweep.php allowed me to generate audio signals
@@ -17,6 +18,7 @@ from struct import unpack
 import numpy as NP
 import os as OS
 import sys as SYS
+from cli_visualizer import cli_visualize as CLIV
 
 
 
@@ -98,10 +100,10 @@ def GET_LVLs(data, BLOCKS,SAMPLING_RATE):
    # Find average 'amplitude' for specific frequency ranges in Hz
    AMP = NP.abs(FFT)   #absolute
    
-   FREQ_DOMAIN[0]= int(NP.mean(AMP[piff(0):piff(30):1]))                    # 0  
-   FREQ_DOMAIN[1]= int(NP.mean(AMP[piff(35)  :piff(100):1]))               # 40 -
-   FREQ_DOMAIN[2]= int(NP.mean(AMP[piff(150)  :piff(300):1]))              # 47 
-   FREQ_DOMAIN[3]= int(NP.mean(AMP[piff(320)  :piff(500):1]))              # 94 - 
+   FREQ_DOMAIN[0]= int(NP.mean(AMP[piff(0):piff(30):1]))                   # 0  
+   FREQ_DOMAIN[1]= int(NP.mean(AMP[piff(35)  :piff(100):1]))               # 
+   FREQ_DOMAIN[2]= int(NP.mean(AMP[piff(150)  :piff(300):1]))              # 
+   FREQ_DOMAIN[3]= int(NP.mean(AMP[piff(320)  :piff(500):1]))              #  
    FREQ_DOMAIN[4]= int(NP.mean(AMP[piff(600) :piff(1000):1]))              # 1
    FREQ_DOMAIN[5]= int(NP.mean(AMP[piff(1100) :piff(2500):1]))             
    FREQ_DOMAIN[6]= int(NP.mean(AMP[piff(3000) :piff(5000):1]))             
@@ -132,36 +134,14 @@ data = WAVSAMPLE.readframes(BLOCKS)
 try:
     print("Initializing main loop (Exit with Ctrl + C)")
     print("\n\n")
-    # print lables: Frequency Level Vector.....f0 range, f1 range,....,etc
-    print("                           0-30         30          150          320           600          1100         3k           5k           7.1k         10.1k")
-    print("Freq. LVL Vect             f0R          f1R         f2R          f3R           f4R          f5R          f6R          f7R          f8R          f9R")
+
     
     # Main loop (Exit with Ctrl + C)
     while data!='':
-       ALSA_OUT.write(data)
-       FREQ_DOMAIN=GET_LVLs(data, BLOCKS,SAMPLING_RATE)     
-
-       for i in range (0,FREQ_DOMAIN_NUM_RANGES):
-            if FREQ_DOMAIN[i] == 0:
-                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[0]
-            elif FREQ_DOMAIN[i] <=4 and FREQ_DOMAIN[i] > 0:
-                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[1]
-            elif FREQ_DOMAIN[i] <=8 and FREQ_DOMAIN[i] > 4:
-                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[2]
-            elif FREQ_DOMAIN[i] <=12 and FREQ_DOMAIN[i] > 8:
-                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[3]
-            elif FREQ_DOMAIN[i] <=16 and FREQ_DOMAIN[i] > 12:
-                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[4]
-            elif FREQ_DOMAIN[i] <=FREQ_DOMAIN_CEILING and FREQ_DOMAIN[i] > 16:
-                 FREQ_LVL_CUR[i]=FREQ_LVL_REF[5]
-
-       OS.system("echo -en " + str(FREQ_DOMAIN) + str(FREQ_LVL_CUR[0]) + str(FREQ_LVL_CUR[1]) + str(FREQ_LVL_CUR[2]) + str(FREQ_LVL_CUR[3]) +str(FREQ_LVL_CUR[4]) +str(FREQ_LVL_CUR[5]) +str(FREQ_LVL_CUR[6]) + str(FREQ_LVL_CUR[7]) + str(FREQ_LVL_CUR[8])+ str(FREQ_LVL_CUR[9]) + "\r")
-    
-       
-     
-    
-
-       data = WAVSAMPLE.readframes(BLOCKS)
+       ALSA_OUT.write(data)     # Output the original file
+       FREQ_DOMAIN=GET_LVLs(data, BLOCKS,SAMPLING_RATE)     # Calculate FFT of the current data-block     
+       CLIV(FREQ_DOMAIN)                                   # Visualize the current Frequency Domain on CLI
+       data = WAVSAMPLE.readframes(BLOCKS)                  # Read the next data-block
 
 
     WAVESAMPLE.close()
